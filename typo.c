5ed2e3c84b2243465f0f4e0746fdbb75ca2817d5
root <root@debian>
2017-05-28 16:47:16 +0200

    lire fichier ligne par ligne

---
+++ typo.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main() {
+	FILE *fp;
+	char str[60];
+	char cop[60];
+	/* ouverture du fichier */
+	fp = fopen("file.txt" , "r");
+
+	if(fp == NULL) {
+		perror("Error opening file");
+		return(-1);
+	}
+
+	while (fgets (str, 60, fp) != NULL) {
+		printf("%s\n", str);
+		char copie = scanf("%s", cop);
+	//	if(str == cop) {		
+	//	}
+	}
+
+	/* fermeture du fichier */
+	fclose(fp);
+	return 0;
+} 
