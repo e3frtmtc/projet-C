697481d2a1ababee1b2c9e7ac89c9c4b69850165
Remi BUI <buir@pc5301-test.esiee.fr>
2017-05-29 15:56:23 +0200

    ex1

--- typo.c
+++ typo.c
@@ -1,9 +1,14 @@
 ﻿#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
 
 int main() {
 	FILE *fp;
-	char str[60];
-	char cop[60];
+	char str[60], cop[60], copie;
+	int comp, faute, mot;
+	time_t t1, t2;
+
 	/* ouverture du fichier */
 	fp = fopen("file.txt" , "r");
 
@@ -11,15 +16,35 @@ int main() {
 		perror("Error opening file");
 		return(-1);
 	}
-
+	t1 = time(NULL);
 	while (fgets (str, 60, fp) != NULL) {
-		printf("%s\n", str);
-		char copie = scanf("%s", cop);
-	//	if(str == cop) {		
-	//	}
+		int i = 0;
+		printf("%s", str);
+		copie = *fgets(cop, 60, stdin);	
+		printf("\n");
+		
+
+		while (i == 0) {	
+			comp = strcmp(str, cop);
+			if ( comp == 0) {				
+				printf("Alleeeez ! Mot suivant :\n");
+				i++;
+			}
+			else {
+				printf("Perdu ! Recommence :\n");
+				printf("%s", str);		
+				copie = *fgets(cop, 60, stdin);	
+				printf("\n");
+				faute++;
+			}
+
+		}
 	}
+	t2 = time(NULL);
 
 	/* fermeture du fichier */
 	fclose(fp);
+	printf("Vous avez fait %d faute(s).\n", faute);
+	printf("Vous avez mis : %.1f secondes pour ecrire %d mots.\n", difftime(t2, t1), mot);
 	return 0;
 } 
